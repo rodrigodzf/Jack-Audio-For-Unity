f00bc2d1a7660492bbde05e86ba25a92f4d68a0e
Rodrigo Diaz <rodrigodzf@gmail.com>
2016-10-19 12:28:30 +0200

    cleanup

--- JackForUnity/InternalJackClient.h
+++ JackForUnity/InternalJackClient.h
@@ -1,3 +1,21 @@
+// Copyright (C) 2016  Rodrigo Diaz
+// 
+// This file is part of JackAudioUnity.
+// 
+// JackAudioUnity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// JackAudioUnity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with JackAudioUnity.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
 #include <jack/jack.h>
 #include <jack/ringbuffer.h>
 #include <jack/types.h>
